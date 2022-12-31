9a06d938fdd7160d7bba5146abdbca102d1d742a
Hidekazu Tanaka <tanakahidek@gmail.com>
2022-12-31 16:08:55 +0900

    fix task interface

--- e2e/tasks_test.go
+++ e2e/tasks_test.go
@@ -9,31 +9,48 @@ import (
 
 const BaseUrl = "http://localhost:8080"
 
+type Task struct {
+	Id      string `json:"id"`
+	Subject string `json:"subject"`
+	Link    string `json:"link"`
+	Body    string `json:"body"`
+}
+
+type Re struct {
+	Data []Task `json:"data"`
+}
+
 func TestTasks(t *testing.T) {
 	r, err := http.Get(BaseUrl + "/tasks")
 	if err != nil {
 		t.Errorf("http get err should be nil: %v", err)
 	}
 	defer r.Body.Close()
-	var j []map[string]string
+	//var j map[string]any
+	var j Re
 	if err := json.NewDecoder(r.Body).Decode(&j); err != nil {
 		t.Errorf("json decode err should be nil: %v", err)
 	}
-	expected := []map[string]string{
-		{
-			"subject": "subject1",
-			"body":    "body1",
-			"link":    "https://example.com",
-		},
-		{
-			"subject": "subject2",
-			"body":    "body2",
-			"link":    "https://example.com",
-		},
-		{
-			"subject": "subject3",
-			"body":    "body3",
-			"link":    "https://example.com",
+	expected := Re{
+		Data: []Task{
+			{
+				Id:      "id1",
+				Subject: "subject1",
+				Body:    "body1",
+				Link:    "https://example.com",
+			},
+			{
+				Id:      "id2",
+				Subject: "subject2",
+				Body:    "body2",
+				Link:    "https://example.com",
+			},
+			{
+				Id:      "id3",
+				Subject: "subject3",
+				Body:    "body3",
+				Link:    "https://example.com",
+			},
 		},
 	}
 	assert.Equal(t, j, expected)
--- handler/main.go
+++ handler/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Task struct {
+	Id      string `json:"id"`
 	Subject string `json:"subject"`
 	Link    string `json:"link"`
 	Body    string `json:"body"`
@@ -14,14 +15,17 @@ type Task struct {
 
 func Tasks(w http.ResponseWriter, r *http.Request) {
 	tasks := []Task{
-		{Subject: "subject1", Link: "https://example.com", Body: "body1"},
-		{Subject: "subject2", Link: "https://example.com", Body: "body2"},
-		{Subject: "subject3", Link: "https://example.com", Body: "body3"},
+		{Id: "id1", Subject: "subject1", Link: "https://example.com", Body: "body1"},
+		{Id: "id2", Subject: "subject2", Link: "https://example.com", Body: "body2"},
+		{Id: "id3", Subject: "subject3", Link: "https://example.com", Body: "body3"},
+	}
+	res := map[string][]Task{
+		"data": tasks,
 	}
 	w.Header().Set("Content-Type", "application/json")
 	switch r.Method {
 	case http.MethodGet:
-		if err := json.NewEncoder(w).Encode(tasks); err != nil {
+		if err := json.NewEncoder(w).Encode(res); err != nil {
 			http.Error(w, fmt.Sprintf(`{"status": "%s"}`, err), http.StatusInternalServerError)
 			return
 		}
